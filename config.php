c81532ab1f2559e62bc159eef455560bba5e2d10
José Capewa <capewajose@gmail.com>
2024-09-17 22:33:47 +0000

    change hostname

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 
-  define( 'host', 'socartao.com' );
+  define( 'host', 'ws5.angoweb.net' );
   define( 'user', 'socartao_jose' );
   define( 'pass', '}z{~460YZ(Y9' );
   define( 'dbname', 'socartao_capewa_oauth' );
