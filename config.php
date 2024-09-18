05d4cc0e84a84e2f23ec7c8fb66d0b94910f614d
José Capewa <capewajose@gmail.com>
2024-09-18 08:21:45 +0000

    back to normal host

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 
-  define( 'host', 'ws5.angoweb.net' );
+  define( 'host', 'cloud5.angoweb.biz' );
   define( 'user', 'socartao_jose' );
   define( 'pass', '}z{~460YZ(Y9' );
   define( 'dbname', 'socartao_capewa_oauth' );
